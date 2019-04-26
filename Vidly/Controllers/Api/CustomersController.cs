8bb48b237b1d063d1dfe76d7cbf66321adff6460
hugo.marcelo <hugo.marcelo@lg.com.br>
2019-04-26 18:14:30 -0300

    Commit - Criação do form New Rental

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -21,10 +21,14 @@ namespace Vidly.Controllers.Api
         }
 
         // GET /api/customers
-        public IEnumerable<CustomerDto> GetCustomers()
+        public IEnumerable<CustomerDto> GetCustomers(string query = null)
         {
-            return _context.Customers
-                .Include(c => c.MembershipType)
+            var customersQuery = _context.Customers.Include(c => c.MembershipType);
+
+            if (!String.IsNullOrWhiteSpace(query))
+                customersQuery = customersQuery.Where(c => c.Name.Contains(query));
+
+            return customersQuery
                 .ToList()
                 .Select(Mapper.Map<Customer, CustomerDto>);
         } 
